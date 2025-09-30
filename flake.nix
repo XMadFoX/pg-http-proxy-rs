282d8f8818cdf1fcf329818ec41add9bb5630f59
MadFox <madfox@laserflare.net>
2025-09-30 04:22:58 +0400

    build: use system arch & don't require impure

--- flake.nix
+++ flake.nix
@@ -13,71 +13,82 @@
       flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        rustPkgs = pkgs.rustPlatform;
-        finalPackage = self.packages.${system}.pgHttpProxy;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            openssl
-            pkg-config
-            clippy
-          ];
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        };
+    let
+      systems = flake-utils.lib.defaultSystems;
+      forAllSystems = nixpkgs.lib.genAttrs systems;
 
-        packages.pgHttpProxy = rustPkgs.buildRustPackage rec {
-          cargoBuildFlags = [ "" ];
-          pname = "pg-http-proxy";
-          version = "0.1.0";
-          src = ./.;
+      packagesFor =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          rustPkgs = pkgs.rustPlatform;
+          pgHttpProxy = rustPkgs.buildRustPackage rec {
+            cargoBuildFlags = [ "" ];
+            pname = "pg-http-proxy";
+            version = "0.1.0";
+            src = ./.;
 
-          cargoLock.lockFile = ./Cargo.lock;
+            cargoLock.lockFile = ./Cargo.lock;
 
-          buildInputs = [
-            pkgs.openssl
-            pkgs.pkg-config
-          ];
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-          OPENSSL_DIR = pkgs.lib.getDev pkgs.openssl;
-          OPENSSL_LIBS_DIR = pkgs.lib.getLib pkgs.openssl;
-          OPENSSL_NO_VENDOR = 1;
-          OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
-        };
-
-        # Docker image output
-        dockerImage =
-          let
-            finalPackage = self.packages.${system}.pgHttpProxy;
-          in
-          pkgs.dockerTools.buildImage {
+            buildInputs = [
+              pkgs.openssl
+              pkgs.pkg-config
+            ];
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            OPENSSL_DIR = pkgs.lib.getDev pkgs.openssl;
+            OPENSSL_LIBS_DIR = pkgs.lib.getLib pkgs.openssl;
+            OPENSSL_NO_VENDOR = 1;
+            OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
+          };
+        in
+        rec {
+          inherit pgHttpProxy;
+          dockerImage = pkgs.dockerTools.buildImage {
             name = "pg-http-proxy-rs";
             tag = "latest";
 
-            # Copy the built binary into the image
-            copyToRoot = [
-              self.packages.${system}.pgHttpProxy
-            ];
+            copyToRoot = [ pgHttpProxy ];
 
             config = {
-              # Entrypoint (or Cmd) pointing to your binary
-              Entrypoint = [ "${finalPackage}/bin/pg-http-proxy" ];
+              Entrypoint = [ "${pgHttpProxy}/bin/pg-http-proxy" ];
               ExposedPorts = {
                 "8080/tcp" = { };
               };
-              # Working directory, environment, etc can go here
             };
 
             # TODO: build on more minimal base image
             # baseImage = someBaseImage;
           };
-      }
-    );
+
+          default = pgHttpProxy;
+        };
+
+      devShellsFor =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              openssl
+              pkg-config
+              clippy
+            ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        };
+    in
+    {
+      packages = forAllSystems packagesFor;
+      devShells = forAllSystems devShellsFor;
+      dockerImage =
+        let
+          system = builtins.currentSystem;
+        in
+        self.packages.${system}.dockerImage;
+    };
 }
