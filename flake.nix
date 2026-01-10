e8b116320e1c48b7feb1b0b67d3013afb5573381
MadFox <madfox@laserflare.net>
2026-01-11 00:05:49 +0400

    build: improve docker build caching

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
@@ -11,6 +12,7 @@
       self,
       nixpkgs,
       flake-utils,
+      crane,
       ...
     }:
     let
@@ -21,25 +23,42 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
-          rustPkgs = pkgs.rustPlatform;
-          pgHttpProxy = rustPkgs.buildRustPackage rec {
-            cargoBuildFlags = [ "" ];
-            pname = "pg-http-proxy";
-            version = "0.1.3";
-            src = ./.;
+          craneLib = crane.mkLib pkgs;
 
-            cargoLock.lockFile = ./Cargo.lock;
+          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+
+          # Filter source to include only necessary files (ignoring flake.nix, etc.)
+          src = craneLib.cleanCargoSource ./.;
+
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
 
             buildInputs = [
               pkgs.openssl
+            ];
+
+            nativeBuildInputs = [
               pkgs.pkg-config
             ];
+
+            # SSL Env vars
             PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-            OPENSSL_DIR = pkgs.lib.getDev pkgs.openssl;
-            OPENSSL_LIBS_DIR = pkgs.lib.getLib pkgs.openssl;
+            OPENSSL_DIR = "${pkgs.lib.getDev pkgs.openssl}";
+            OPENSSL_LIBS_DIR = "${pkgs.lib.getLib pkgs.openssl}";
             OPENSSL_NO_VENDOR = 1;
             OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
           };
+
+          # Build *only* the dependencies (this will be cached)
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          # Build the actual crate
+          pgHttpProxy = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            pname = manifest.name;
+            version = manifest.version;
+          });
         in
         rec {
           inherit pgHttpProxy;
@@ -90,6 +109,14 @@
               clippy
             ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+            # Ensure env vars are present in shell too
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            OPENSSL_DIR = "${pkgs.lib.getDev pkgs.openssl}";
+
+            # Other env vars for convenience
+            OPENSSL_NO_VENDOR = 1;
+            OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
           };
         };
     in
